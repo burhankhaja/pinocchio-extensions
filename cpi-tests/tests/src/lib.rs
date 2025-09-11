3dd1f3fed6005ecd2a1593184ceb7f471353c1c1
M. Daeva <embedmad@gmail.com>
2025-09-11 20:55:10 +0300

    removed unused code

--- cpi-tests/tests/src/lib.rs
+++ cpi-tests/tests/src/lib.rs
@@ -2,15 +2,12 @@
 pub mod initialize_mint;
 #[cfg(test)]
 pub mod token_group;
-// #[cfg(test)]
-// pub mod token_metadata;
 
 pub mod helpers {
     pub mod extensions {
         pub mod token_2022 {
             pub mod initialize_mint;
             pub mod token_group;
-            pub mod token_metadata;
         }
     }
 
