0a259baf3e4249c967bdc6f54bd4b4febb055853
burhankhaja <karhazemail@gmail.com>
2025-10-27 13:42:44 +0000

    CHECKPOINT 3 : (all test helpers completed)

--- cpi-tests/tests/src/helpers/extensions/token_2022/token_metadata.rs
+++ cpi-tests/tests/src/helpers/extensions/token_2022/token_metadata.rs
@@ -7,8 +7,10 @@ use {
     solana_address::Address,
 };
 
+use spl_token_metadata_interface::state::Field;
+
 pub trait Token2022TokenMetadataExtension {
-    fn token_2022_try_initialize_token_metadata(
+    fn token_2022_try_token_metadata_initialize(
         &mut self,
         target: Target,
         sender: AppUser,
@@ -20,10 +22,48 @@ pub trait Token2022TokenMetadataExtension {
         symbol: String,
         uri: String,
     ) -> TestResult<TransactionMetadata>;
+
+    fn token_2022_try_token_metadata_update_field(
+        &mut self,
+        target: Target,
+        sender: AppUser,
+        metadata: &Address,
+        update_authority: &Address,
+        field: Field,
+        value: String,
+    ) -> TestResult<TransactionMetadata>;
+
+    fn token_2022_try_token_metadata_remove_key(
+        &mut self,
+        target: Target,
+        sender: AppUser,
+        metadata: &Address,
+        update_authority: &Address,
+        key: String,
+        idempotent: bool,
+    ) -> TestResult<TransactionMetadata>;
+
+    fn token_2022_try_token_metadata_update_authority(
+        &mut self,
+        target: Target,
+        sender: AppUser,
+        metadata: &Address,
+        current_authority: &Address,
+        new_authority: spl_pod::optional_keys::OptionalNonZeroPubkey,
+    ) -> TestResult<TransactionMetadata>;
+
+    fn token_2022_try_token_metadata_emit(
+        &mut self,
+        target: Target,
+        sender: AppUser,
+        metadata: &Address,
+        start: Option<u64>,
+        end: Option<u64>,
+    ) -> TestResult<TransactionMetadata>;
 }
 
 impl Token2022TokenMetadataExtension for App {
-    fn token_2022_try_initialize_token_metadata(
+    fn token_2022_try_token_metadata_initialize(
         &mut self,
         target: Target,
         sender: AppUser,
@@ -54,6 +94,242 @@ impl Token2022TokenMetadataExtension for App {
             name,
             symbol,
             uri,
+        );
+
+        let additional_accounts = [solana_instruction::AccountMeta::new_readonly(
+            token_2022_program,
+            false,
+        )];
+
+        let mut ix_legacy = solana_instruction::Instruction {
+            program_id: addr_to_sol_pubkey(&ix.program_id),
+            accounts: ix
+                .accounts
+                .into_iter()
+                .map(|x| solana_instruction::AccountMeta {
+                    pubkey: addr_to_sol_pubkey(&x.pubkey),
+                    is_signer: x.is_signer,
+                    is_writable: x.is_writable,
+                })
+                .collect(),
+            data: ix.data,
+        };
+
+        if let Target::Proxy = target {
+            ix_legacy.program_id = token_2022_proxy;
+            ix_legacy.accounts.extend_from_slice(&additional_accounts);
+        }
+
+        send_tx(
+            &mut self.litesvm,
+            &[ix_legacy],
+            signers,
+            self.is_log_displayed,
+        )
+    }
+
+    // update field
+    fn token_2022_try_token_metadata_update_field(
+        &mut self,
+        target: Target,
+        sender: AppUser,
+        metadata: &Address,
+        update_authority: &Address,
+        field: Field,
+        value: String,
+    ) -> TestResult<TransactionMetadata> {
+        let ProgramId {
+            token_2022_program,
+            token_2022_proxy,
+            ..
+        } = self.program_id;
+
+        let token_2022_program_addr = Address::new_from_array(token_2022_program.to_bytes());
+
+        let signers = &[sender.keypair()];
+
+        let ix = spl_token_metadata_interface::instruction::update_field(
+            &token_2022_program_addr,
+            metadata,
+            update_authority,
+            field,
+            value,
+        );
+
+        let additional_accounts = [solana_instruction::AccountMeta::new_readonly(
+            token_2022_program,
+            false,
+        )];
+
+        let mut ix_legacy = solana_instruction::Instruction {
+            program_id: addr_to_sol_pubkey(&ix.program_id),
+            accounts: ix
+                .accounts
+                .into_iter()
+                .map(|x| solana_instruction::AccountMeta {
+                    pubkey: addr_to_sol_pubkey(&x.pubkey),
+                    is_signer: x.is_signer,
+                    is_writable: x.is_writable,
+                })
+                .collect(),
+            data: ix.data,
+        };
+
+        if let Target::Proxy = target {
+            ix_legacy.program_id = token_2022_proxy;
+            ix_legacy.accounts.extend_from_slice(&additional_accounts);
+        }
+
+        send_tx(
+            &mut self.litesvm,
+            &[ix_legacy],
+            signers,
+            self.is_log_displayed,
+        )
+    }
+
+    // remove key
+    fn token_2022_try_token_metadata_remove_key(
+        &mut self,
+        target: Target,
+        sender: AppUser,
+        metadata: &Address,
+        update_authority: &Address,
+        key: String,
+        idempotent: bool,
+    ) -> TestResult<TransactionMetadata> {
+        let ProgramId {
+            token_2022_program,
+            token_2022_proxy,
+            ..
+        } = self.program_id;
+
+        let token_2022_program_addr = Address::new_from_array(token_2022_program.to_bytes());
+
+        let signers = &[sender.keypair()];
+
+        let ix = spl_token_metadata_interface::instruction::remove_key(
+            &token_2022_program_addr,
+            metadata,
+            update_authority,
+            key,
+            idempotent,
+        );
+
+        let additional_accounts = [solana_instruction::AccountMeta::new_readonly(
+            token_2022_program,
+            false,
+        )];
+
+        let mut ix_legacy = solana_instruction::Instruction {
+            program_id: addr_to_sol_pubkey(&ix.program_id),
+            accounts: ix
+                .accounts
+                .into_iter()
+                .map(|x| solana_instruction::AccountMeta {
+                    pubkey: addr_to_sol_pubkey(&x.pubkey),
+                    is_signer: x.is_signer,
+                    is_writable: x.is_writable,
+                })
+                .collect(),
+            data: ix.data,
+        };
+
+        if let Target::Proxy = target {
+            ix_legacy.program_id = token_2022_proxy;
+            ix_legacy.accounts.extend_from_slice(&additional_accounts);
+        }
+
+        send_tx(
+            &mut self.litesvm,
+            &[ix_legacy],
+            signers,
+            self.is_log_displayed,
+        )
+    }
+
+    // update authority
+    fn token_2022_try_token_metadata_update_authority(
+        &mut self,
+        target: Target,
+        sender: AppUser,
+        metadata: &Address,
+        current_authority: &Address,
+        new_authority: spl_pod::optional_keys::OptionalNonZeroPubkey,
+    ) -> TestResult<TransactionMetadata> {
+        let ProgramId {
+            token_2022_program,
+            token_2022_proxy,
+            ..
+        } = self.program_id;
+
+        let token_2022_program_addr = Address::new_from_array(token_2022_program.to_bytes());
+
+        let signers = &[sender.keypair()];
+
+        let ix = spl_token_metadata_interface::instruction::update_authority(
+            &token_2022_program_addr,
+            metadata,
+            current_authority,
+            new_authority,
+        );
+
+        let additional_accounts = [solana_instruction::AccountMeta::new_readonly(
+            token_2022_program,
+            false,
+        )];
+
+        let mut ix_legacy = solana_instruction::Instruction {
+            program_id: addr_to_sol_pubkey(&ix.program_id),
+            accounts: ix
+                .accounts
+                .into_iter()
+                .map(|x| solana_instruction::AccountMeta {
+                    pubkey: addr_to_sol_pubkey(&x.pubkey),
+                    is_signer: x.is_signer,
+                    is_writable: x.is_writable,
+                })
+                .collect(),
+            data: ix.data,
+        };
+
+        if let Target::Proxy = target {
+            ix_legacy.program_id = token_2022_proxy;
+            ix_legacy.accounts.extend_from_slice(&additional_accounts);
+        }
+
+        send_tx(
+            &mut self.litesvm,
+            &[ix_legacy],
+            signers,
+            self.is_log_displayed,
+        )
+    }
+
+    // emit
+    fn token_2022_try_token_metadata_emit(
+        &mut self,
+        target: Target,
+        sender: AppUser,
+        metadata: &Address,
+        start: Option<u64>,
+        end: Option<u64>,
+    ) -> TestResult<TransactionMetadata> {
+        let ProgramId {
+            token_2022_program,
+            token_2022_proxy,
+            ..
+        } = self.program_id;
+
+        let token_2022_program_addr = Address::new_from_array(token_2022_program.to_bytes());
+
+        let signers = &[sender.keypair()];
+
+        let ix = spl_token_metadata_interface::instruction::emit(
+            &token_2022_program_addr,
+            metadata,
+            start,
+            end,
         );
 
         let additional_accounts = [solana_instruction::AccountMeta::new_readonly(
--- cpi-tests/tests/src/token_metadata.rs
+++ cpi-tests/tests/src/token_metadata.rs
@@ -10,11 +10,16 @@ use {
             core::{extension, App},
             types::{pin_pubkey_to_addr, AppUser, PinPubkey, Target, TestResult},
         },
-    }, pinocchio::pubkey::Pubkey, pretty_assertions::assert_eq, solana_keypair::Keypair, solana_signer::Signer, spl_pod::optional_keys::OptionalNonZeroPubkey, spl_token_2022_interface::extension::{metadata_pointer::MetadataPointer, ExtensionType}
+    },
+    pinocchio::pubkey::Pubkey,
+    pretty_assertions::assert_eq,
+    solana_keypair::Keypair,
+    solana_signer::Signer,
+    spl_pod::optional_keys::OptionalNonZeroPubkey,
+    spl_token_2022_interface::extension::{metadata_pointer::MetadataPointer, ExtensionType},
 };
 
 use spl_token_metadata_interface::state::TokenMetadata;
-
 
 #[test]
 fn test_token_metadata() -> TestResult<()> {
@@ -101,7 +106,7 @@ fn test_token_metadata() -> TestResult<()> {
     app.litesvm
         .airdrop(&solana_pubkey::Pubkey::from(*mint_pubkey), 2_000_000);
 
-    app.token_2022_try_initialize_token_metadata(
+    app.token_2022_try_token_metadata_initialize(
         Target::Spl,
         AppUser::Admin,
         &pin_pubkey_to_addr(&mint_pubkey),
@@ -179,7 +184,6 @@ fn proxy_test_token_metadata() -> TestResult<()> {
         freeze_authority.as_ref(),
     )?;
 
-
     let admin_pubkey = AppUser::Admin.pubkey();
 
     // Define Token metadata
@@ -192,17 +196,13 @@ fn proxy_test_token_metadata() -> TestResult<()> {
         additional_metadata: vec![("description".to_string(),"only possible on Solana".to_string())]
     };
 
-
     let mint_raw_data_before = extension::get_account_data(&app, &mint_pubkey)?;
-
-
-
 
     // note : fund `metadata` bit more to avoid litesvm rent errors
     app.litesvm
         .airdrop(&solana_pubkey::Pubkey::from(*mint_pubkey), 2_000_000);
 
-    app.token_2022_try_initialize_token_metadata(
+    app.token_2022_try_token_metadata_initialize(
         Target::Proxy,
         AppUser::Admin,
         &pin_pubkey_to_addr(&mint_pubkey),
@@ -214,24 +214,41 @@ fn proxy_test_token_metadata() -> TestResult<()> {
         token_metadata.uri.to_string(),
     )?;
 
-
     Ok(())
 }
 
+#[ignore]
 #[test]
 fn decode_data() {
-    let token_metadata = [1, 0, 0, 0, 219, 13, 46, 235, 155, 0, 103, 86, 67, 139, 99, 108, 203, 151, 240, 193, 9, 36, 6, 10, 91, 89, 180, 123, 28, 168, 121, 38, 63, 127, 68, 123, 0, 0, 0, 0, 0, 0, 0, 0, 6, 1, 1, 0, 0, 0, 219, 13, 46, 235, 155, 0, 103, 86, 67, 139, 99, 108, 203, 151, 240, 193, 9, 36, 6, 10, 91, 89, 180, 123, 28, 168, 121, 38, 63, 127, 68, 123, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 18, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 96, 188, 192, 176, 62, 209, 111, 88, 22, 76, 163, 199, 172, 235, 9, 22, 8, 255, 8, 38, 167, 190, 14, 9, 91, 96, 73, 221, 165, 35, 172, 169, 19, 0, 191, 0, 219, 13, 46, 235, 155, 0, 103, 86, 67, 139, 99, 108, 203, 151, 240, 193, 9, 36, 6, 10, 91, 89, 180, 123, 28, 168, 121, 38, 63, 127, 68, 123, 96, 188, 192, 176, 62, 209, 111, 88, 22, 76, 163, 199, 172, 235, 9, 22, 8, 255, 8, 38, 167, 190, 14, 9, 91, 96, 73, 221, 165, 35, 172, 169, 4, 0, 0, 0, 79, 80, 79, 83, 3, 0, 0, 0, 79, 80, 83, 104, 0, 0, 0, 104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 115, 111, 108, 97, 110, 97, 45, 100, 101, 118, 101, 108, 111, 112, 101, 114, 115, 47, 111, 112, 111, 115, 45, 97, 115, 115, 101, 116, 47, 109, 97, 105, 110, 47, 97, 115, 115, 101, 116, 115, 47, 68, 101, 118, 101, 108, 111, 112, 101, 114, 80, 111, 114, 116, 97, 108, 47, 109, 101, 116, 97, 100, 97, 116, 97, 46, 106, 115, 111, 110, 0, 0, 0, 0];
- 
-    println!("data from 165 index: {:?}", &token_metadata[165..165+8]); //[1, 18, 0, 64, 0, 0, 0, 0] // could be initialize
+    let token_metadata = [
+        1, 0, 0, 0, 219, 13, 46, 235, 155, 0, 103, 86, 67, 139, 99, 108, 203, 151, 240, 193, 9, 36,
+        6, 10, 91, 89, 180, 123, 28, 168, 121, 38, 63, 127, 68, 123, 0, 0, 0, 0, 0, 0, 0, 0, 6, 1,
+        1, 0, 0, 0, 219, 13, 46, 235, 155, 0, 103, 86, 67, 139, 99, 108, 203, 151, 240, 193, 9, 36,
+        6, 10, 91, 89, 180, 123, 28, 168, 121, 38, 63, 127, 68, 123, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 18, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 96, 188, 192, 176, 62, 209,
+        111, 88, 22, 76, 163, 199, 172, 235, 9, 22, 8, 255, 8, 38, 167, 190, 14, 9, 91, 96, 73,
+        221, 165, 35, 172, 169, 19, 0, 191, 0, 219, 13, 46, 235, 155, 0, 103, 86, 67, 139, 99, 108,
+        203, 151, 240, 193, 9, 36, 6, 10, 91, 89, 180, 123, 28, 168, 121, 38, 63, 127, 68, 123, 96,
+        188, 192, 176, 62, 209, 111, 88, 22, 76, 163, 199, 172, 235, 9, 22, 8, 255, 8, 38, 167,
+        190, 14, 9, 91, 96, 73, 221, 165, 35, 172, 169, 4, 0, 0, 0, 79, 80, 79, 83, 3, 0, 0, 0, 79,
+        80, 83, 104, 0, 0, 0, 104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116,
+        104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47,
+        115, 111, 108, 97, 110, 97, 45, 100, 101, 118, 101, 108, 111, 112, 101, 114, 115, 47, 111,
+        112, 111, 115, 45, 97, 115, 115, 101, 116, 47, 109, 97, 105, 110, 47, 97, 115, 115, 101,
+        116, 115, 47, 68, 101, 118, 101, 108, 111, 112, 101, 114, 80, 111, 114, 116, 97, 108, 47,
+        109, 101, 116, 97, 100, 97, 116, 97, 46, 106, 115, 111, 110, 0, 0, 0, 0,
+    ];
+
+    println!("data from 165 index: {:?}", &token_metadata[165..165 + 8]); //[1, 18, 0, 64, 0, 0, 0, 0] // could be initialize
 }
-
-
 
 // debug____discriminators___directly
 #[ignore]
 #[test]
 fn test_discriminators() {
-
     use solana_address::Address;
 
     let mut app = App::new(true);
@@ -253,7 +270,7 @@ fn test_discriminators() {
         additional_metadata: vec![("description".to_string(),"only possible on Solana".to_string())]
     };
 
-    // app.token_2022_try_initialize_token_metadata(
+    // app.token_2022_try_token_metadata_initialize(
     //     Target::Proxy,
     //     AppUser::Admin,
     //     &pin_pubkey_to_addr(&mint_pubkey),
@@ -266,84 +283,104 @@ fn test_discriminators() {
     // )?;
 
     use crate::helpers::suite::core::ProgramId;
-       let ProgramId {
-            token_2022_program,
-            token_2022_proxy,
-            ..
-        } = app.program_id;
+    let ProgramId {
+        token_2022_program,
+        token_2022_proxy,
+        ..
+    } = app.program_id;
 
-        let token_2022_program_addr = Address::new_from_array(token_2022_program.to_bytes());
+    let token_2022_program_addr = Address::new_from_array(token_2022_program.to_bytes());
 
-        // let signers = &[sender.keypair()];
-        let signers = &[AppUser::Admin.keypair()];
+    // let signers = &[sender.keypair()];
+    let signers = &[AppUser::Admin.keypair()];
 
-        let ix = spl_token_metadata_interface::instruction::initialize(
-            &token_2022_program_addr,
-            &pin_pubkey_to_addr(&mint_pubkey),
-            &pin_pubkey_to_addr(&admin_pubkey),
-            &pin_pubkey_to_addr(&mint_pubkey),
-            &pin_pubkey_to_addr(&admin_pubkey),
+    let ix = spl_token_metadata_interface::instruction::initialize(
+        &token_2022_program_addr,
+        &pin_pubkey_to_addr(&mint_pubkey),
+        &pin_pubkey_to_addr(&admin_pubkey),
+        &pin_pubkey_to_addr(&mint_pubkey),
+        &pin_pubkey_to_addr(&admin_pubkey),
         token_metadata.name.to_string(),
         token_metadata.symbol.to_string(),
         token_metadata.uri.to_string(),
-        );
+    );
 
-     let ix_data = ix.data;
-     println!("ix_data : {:?}", ix_data);
+    let ix_data = ix.data;
+    println!("ix_data : {:?}", ix_data);
 
-     /* Log: 
-     ix_data : [210, 225, 30, 162, 88, 184, 77, 141, 4, 0, 0, 0, 79, 80, 79, 83, 3, 0, 0, 0, 79, 80, 83, 104, 0, 0, 0, 104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 115, 111, 108, 97, 110, 97, 45, 100, 101, 118, 101, 108, 111, 112, 101, 114, 115, 47, 111, 112, 111, 115, 45, 97, 115, 115, 101, 116, 47, 109, 97, 105, 110, 47, 97, 115, 115, 101, 116, 115, 47, 68, 101, 118, 101, 108, 111, 112, 101, 114, 80, 111, 114, 116, 97, 108, 47, 109, 101, 116, 97, 100, 97, 116, 97, 46, 106, 115, 111, 110] */
+    /* Log:
+    ix_data : [210, 225, 30, 162, 88, 184, 77, 141, 4, 0, 0, 0, 79, 80, 79, 83, 3, 0, 0, 0, 79, 80, 83, 104, 0, 0, 0, 104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 115, 111, 108, 97, 110, 97, 45, 100, 101, 118, 101, 108, 111, 112, 101, 114, 115, 47, 111, 112, 111, 115, 45, 97, 115, 115, 101, 116, 47, 109, 97, 105, 110, 47, 97, 115, 115, 101, 116, 115, 47, 68, 101, 118, 101, 108, 111, 112, 101, 114, 80, 111, 114, 116, 97, 108, 47, 109, 101, 116, 97, 100, 97, 116, 97, 46, 106, 115, 111, 110] */
 
-     // may_be_discriminator ::  [210, 225, 30, 162, 88, 184, 77, 141]
+    // may_be_discriminator ::  [210, 225, 30, 162, 88, 184, 77, 141]
 
+    // Unpack to validate
+    use spl_token_metadata_interface::instruction::Initialize;
+    use spl_token_metadata_interface::instruction::TokenMetadataInstruction;
 
-     // Unpack to validate
-     use spl_token_metadata_interface::instruction::TokenMetadataInstruction;
-     use spl_token_metadata_interface::instruction::Initialize;
+    let unpacked_data = TokenMetadataInstruction::unpack(&ix_data).unwrap();
 
-     let unpacked_data = TokenMetadataInstruction::unpack(&ix_data).unwrap();
+    println!("unpacked_data: {:?}", unpacked_data);
 
-     println!("unpacked_data: {:?}", unpacked_data);
+    /*  Log :
+    unpacked_data: Initialize(Initialize { name: "OPOS", symbol: "OPS", uri: "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/DeveloperPortal/metadata.json" })
+    */
 
-     /*  Log : 
-     unpacked_data: Initialize(Initialize { name: "OPOS", symbol: "OPS", uri: "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/DeveloperPortal/metadata.json" })
-     */
-    
     print!("upto ..8: {:?}", &ix_data[..8]);
 
-     let initialize_discriminator = spl_discriminator::ArrayDiscriminator::try_from(&ix_data[..8]).unwrap();
-     println!("Get decoded discriminator from bytes ::: {:?}", initialize_discriminator);
+    let initialize_discriminator =
+        spl_discriminator::ArrayDiscriminator::try_from(&ix_data[..8]).unwrap();
+    println!(
+        "Get decoded discriminator from bytes ::: {:?}",
+        initialize_discriminator
+    );
 
- use spl_discriminator::{discriminator::ArrayDiscriminator, SplDiscriminate};
+    use spl_discriminator::{discriminator::ArrayDiscriminator, SplDiscriminate};
 
     //  use spl_token_metadata_interface::SplDiscriminate;
-     println!("\n real initialize discriminator : {:?} \n", Initialize::SPL_DISCRIMINATOR_SLICE);
+    println!(
+        "\n real initialize discriminator : {:?} \n",
+        Initialize::SPL_DISCRIMINATOR_SLICE
+    );
 }
-
-
 
 // dev :: Important helper test
 #[test]
 fn get_token_metadata_original_discriminators() {
- use spl_discriminator::{discriminator::ArrayDiscriminator, SplDiscriminate};
- use spl_token_metadata_interface::instruction::{Emit, Initialize, RemoveKey, TokenMetadataInstruction, UpdateAuthority, UpdateField};
+    use spl_discriminator::{discriminator::ArrayDiscriminator, SplDiscriminate};
+    use spl_token_metadata_interface::instruction::{
+        Emit, Initialize, RemoveKey, TokenMetadataInstruction, UpdateAuthority, UpdateField,
+    };
 
- println!("\n Initialize discriminator : {:?} \n", Initialize::SPL_DISCRIMINATOR_SLICE);
- println!("\n UpdateField discriminator : {:?} \n", UpdateField::SPL_DISCRIMINATOR_SLICE);
- println!("\n UpdateAuthority discriminator : {:?} \n", UpdateAuthority::SPL_DISCRIMINATOR_SLICE);
- println!("\n RemoveKey discriminator : {:?} \n", RemoveKey::SPL_DISCRIMINATOR_SLICE);
- println!("\n Emit discriminator : {:?} \n", Emit::SPL_DISCRIMINATOR_SLICE);
+    println!(
+        "\n Initialize discriminator : {:?} \n",
+        Initialize::SPL_DISCRIMINATOR_SLICE
+    );
+    println!(
+        "\n UpdateField discriminator : {:?} \n",
+        UpdateField::SPL_DISCRIMINATOR_SLICE
+    );
+    println!(
+        "\n UpdateAuthority discriminator : {:?} \n",
+        UpdateAuthority::SPL_DISCRIMINATOR_SLICE
+    );
+    println!(
+        "\n RemoveKey discriminator : {:?} \n",
+        RemoveKey::SPL_DISCRIMINATOR_SLICE
+    );
+    println!(
+        "\n Emit discriminator : {:?} \n",
+        Emit::SPL_DISCRIMINATOR_SLICE
+    );
 
- /*
- // Logs :: 
-  Initialize discriminator : [210, 225, 30, 162, 88, 184, 77, 141] 
-  UpdateField discriminator : [221, 233, 49, 45, 181, 202, 220, 200] 
-  UpdateAuthority discriminator : [215, 228, 166, 228, 84, 100, 86, 123] 
-  RemoveKey discriminator : [234, 18, 32, 56, 89, 141, 37, 181] 
-  Emit discriminator : [250, 166, 180, 250, 13, 12, 184, 70] 
- 
-  */
- 
+    /*
+    // Logs ::
+     Initialize discriminator : [210, 225, 30, 162, 88, 184, 77, 141]
+     UpdateField discriminator : [221, 233, 49, 45, 181, 202, 220, 200]
+     UpdateAuthority discriminator : [215, 228, 166, 228, 84, 100, 86, 123]
+     RemoveKey discriminator : [234, 18, 32, 56, 89, 141, 37, 181]
+     Emit discriminator : [250, 166, 180, 250, 13, 12, 184, 70]
+
+     */
 }
 
 //@note :: Check how `token_group's` DISCRIMINATOR  is routed via Proxy ; notice that ix_discriminators are encoded in u64 from 8 byte discriminators, you will have to do the same for token_metadata ??
